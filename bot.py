95d0b0d4afd706c0917ffa69deede243203f76e9
Devscryptogo <devscryptogo@proton.me>
2026-01-03 15:25:39 -0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = "8383639485:AAEx1E28f8NUA5Od1k0j5MmNP8CDxKkFv5g"
+import os
+TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
@@ -40,3 +42,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
